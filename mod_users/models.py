a21252d837993388f4bf04a65bec309191c9d093
Majid Sharif <majid.sharif.1362@gmail.com>
2024-09-02 11:43:46 +0330

    fix  db

---
+++ mod_users/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app import db
+
+class User(db.Model):
+    id = Column(Integer(), primary_key=True)
+    username = Column(String(20), nullable=False, unique=True)
+    password = Column(String(128), nullable=False, unique=False)
+    role = Column(Integer(), nullable=False, default=0)
+    full_name = Column(String(30), nullable=True, unique=False)
